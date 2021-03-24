9dc6cdeca0232785caaa876de3c72c878217e5f3
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-03-24 17:17:39 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleAppWithKisuke/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleAppWithKisuke
+{
+    internal class Person
+    {
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        public Person()
+        {
+            this.Statement = "おはよう。";
+        }
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="statement">発言内容の初期値を指定します。</param>
+        public Person(string statement)
+        {
+            this.Statement = statement;
+        }
+        /// <summary>
+        /// 発言内容を取得または設定します。
+        /// </summary>
+        public string Statement { get; set; }
+    }
+}
---
+++ ConsoleAppWithKisuke/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppWithKisuke
+{
+    /// <summary>
+    /// クラス概要
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            Console.WriteLine(person.Statement);
+            person.Statement = "こんにちは。" ;
+            Console.WriteLine(person.Statement);
+
+            Console.WriteLine("何かキーを押すと終了します。");
+            Console.ReadKey();
+        }
+    }
+}
